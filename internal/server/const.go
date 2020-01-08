f36d8f64044469f8793d5c442914a8046f0d996f
Waldemar Quevedo <wally@synadia.com>
2020-01-08 03:17:16 -0600

    Update NATS Server deps
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- internal/server/const.go
+++ internal/server/const.go
@@ -14,7 +14,7 @@
 package server
 
 const (
-	Version             = "0.3.2"
+	Version             = "0.4.0"
 	AppName             = "nats-rest-config-proxy"
 	DefaultPort         = 4567
 	DefaultDataDir      = "data"
