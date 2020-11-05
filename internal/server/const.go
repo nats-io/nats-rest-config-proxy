a2f129c0145c55f5af3a70f88163953b83b55b74
Waldemar Quevedo <wally@synadia.com>
2020-11-05 08:25:21 -0800

    Add JetStream docs
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- internal/server/const.go
+++ internal/server/const.go
@@ -14,7 +14,7 @@
 package server
 
 const (
-	Version             = "0.5.0"
+	Version             = "0.6.0"
 	AppName             = "nats-rest-config-proxy"
 	DefaultPort         = 4567
 	DefaultDataDir      = "data"
